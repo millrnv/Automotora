1b2200a630e09e7df9e5177ac5e90c68a7766908
Nicolas Aguilar <n.aguilar02@ufrontera.cl>
2024-04-19 19:38:53 -0400

    rellenar main

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,7 +1,22 @@
 package controller;
+
+import model.Automotora;
+import ventanas.VentanaMenuBienvenida;
+
 public class Main {
     public static void main(String[] args) {
 
+        inicializar();
+
 
     }
+
+    public static void inicializar(){
+
+        Automotora automotora = new Automotora();
+        VentanaMenuBienvenida ventanaMenuBienvenida = new VentanaMenuBienvenida(automotora);
+        ventanaMenuBienvenida.setVisible(true);
+
+    }
+
 }
